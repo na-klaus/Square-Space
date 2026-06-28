a8c64c45b360c47dff3cc75f2cf9ae565ab2ad94
na-klaus <ansarinavish571@gmail.com>
2026-06-29 04:07:10 +0530

    Remove obsolete Next.js project

--- pages/services.tsx
+++
@@ -1,9 +0,0 @@
-// pages/services.tsx
-import React from 'react';
-import Services from '../src/components/sections/articles/recent.articles'; // Adjust the path as needed
-
-const ServicesPage: React.FC = () => {
-    return <Services />;
-};
-
-export default ServicesPage;
--- src/components/sections/projects/recent.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import css from '../../../styles/scss/sections/projects/recent.module.scss';
-
-export default function GitProjects() {
-  return <div className={css.section}></div>;
-}
